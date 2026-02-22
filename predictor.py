2cbb2aba2034616633fb8dd44b7990f8e399b485
Hridaya Maharjan <hridayamaharjan104@gmail.com>
2026-02-22 14:20:53 +0545

    Added project files

---
+++ predictor.py
@@ -0,0 +1,37 @@
+import joblib
+from fastapi import FastAPI
+import pandas as pd
+from pydantic import BaseModel
+#from fastapi.responses import JSONResponse
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+# ✅ Define input schema
+class HealthData(BaseModel):
+    Age: float
+    SystolicBP: float
+    DiastolicBP: float
+    BS: float
+    BodyTemp: float
+    HeartRate: float
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Maternal Health Risk Predictor"}
+
+
+
+@app.post("/predict")
+def predict(data: HealthData):
+    df = pd.DataFrame([data.model_dump()])
+    df_scaled = scaler.transform(df.values)
+
+    prediction = model.predict(df_scaled)
+
+    # force conversion to pure python
+    result = int(prediction[0])
+    label={0:"low risk",1:"Mid Risk",2:"high risk"}[result]
+
+    return {"predicted_risk":label}
